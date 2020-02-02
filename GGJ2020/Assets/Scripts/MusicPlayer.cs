41efc76003f02783e73bcd3d5ed768c1516dd1a5
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-02 13:05:08 -0700

    Sound folder

---
+++ GGJ2020/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AudioClip AmbientBeat;
+    [SerializeField] AudioClip Music;
+
+    private AudioSource source;
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = AmbientBeat;
+    }
+
+    void Start()
+    {
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LevelManager._instance.objectiveState == 1 && source.clip == AmbientBeat)
+        {
+            source.clip = Music;
+        }
+    }
+}
