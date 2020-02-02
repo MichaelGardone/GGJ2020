13fd75f51ce2d565195cca740514c28774d9998a
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-02 13:44:35 -0700

    powere up and down sound conduit

---
+++ GGJ2020/Assets/ConduitSFX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConduitSFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AudioClip powerUp;
+    [SerializeField] AudioClip powerDown;
+    AudioSource source;
+
+    PowerConduit conduit;
+    bool poweredCheck;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        conduit = GetComponent<PowerConduit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!poweredCheck && conduit.powered)
+        {
+            poweredCheck = true;
+            source.PlayOneShot(powerUp);
+        }
+        else if (poweredCheck && !conduit.powered)
+        {
+            poweredCheck = false;
+            source.PlayOneShot(powerDown);
+        }
+    }
+}
+
