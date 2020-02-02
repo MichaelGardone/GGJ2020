8063de5835ff10147d05bb70b45f86bd944a629c
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-01 21:23:12 -0700

    ui timer

---
+++ GGJ2020/Assets/UITimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UITimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    TextMeshProUGUI timerText;
+
+    private int minutes;
+    private int seconds;
+    private int milliseconds;
+    void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+        float timerLeft = LevelManager._instance.GetCurrentTimeLeft();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LevelManager._instance.timer != 0)
+        {
+            float timerLeft = LevelManager._instance.GetCurrentTimeLeft();
+            minutes = (int)timerLeft / 60;
+            seconds = (int)(timerLeft % 60);
+            milliseconds = (int)((timerLeft % 1) * 100);
+
+            if (milliseconds == 1)
+                timerText.text = ("0" + minutes + ":" + seconds + ":0" + milliseconds);
+            else
+                timerText.text = ("0" + minutes + ":" + seconds + ":" + milliseconds);
+        }
+    }
+}
