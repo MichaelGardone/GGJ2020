82a10e7692128af751e70cd28bd943625e6c93e9
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-02 14:32:27 -0700

    damage wall sfx

--- GGJ2020/Assets/Scripts/Interactable/DamageWall.cs
+++ GGJ2020/Assets/Scripts/Interactable/DamageWall.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class DamageWall : MonoBehaviour
 {
-
+    private AudioSource source;
+    [SerializeField] AudioClip damageSound;
     public int damage = 10;
 
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            source.PlayOneShot(damageSound);
             collision.gameObject.GetComponent<HealthSystem>().ModifyHealth(damage < 0 ? damage : -damage);
             StartCoroutine(Camera.main.GetComponent<CameraShake>().Shake(.15f, .2f));
         }
