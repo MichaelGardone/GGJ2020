d2f3491e82444f44e60312988ac8f9507e8f128c
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-01 23:06:32 -0700

    new win lose canvas and level manager update

---
+++ GGJ2020/Assets/YouWin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouWin : MonoBehaviour
+{
+    bool playing;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LevelManager._instance.hasWon && !playing)
+        {
+            GetComponent<Animator>().Play("LoseScreenAnim");
+            Debug.Log("PlayWinAnim");
+            playing = true;
+        }
+    }
+}
